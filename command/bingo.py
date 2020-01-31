7a814cd2189891ec6e52115faaab7118a21fd956
taiseiyo <53172326+taiseiyo@users.noreply.github.com>
2020-01-31 15:35:26 +0900

    fix bingo.py

--- command/bingo.py
+++ command/bingo.py
@@ -1,50 +1,28 @@
-#!/usr/bin/env python3
+#!/usr/local/bin/python3.7
 import sys
 import pygame
 import pyautogui
-from pygame.locals import KEYUP, KEYDOWN, K_ESCAPE, K_SPACE, QUIT
+from pygame.locals import KEYUP, KEYDOWN, K_ESCAPE, K_SPACE, K_UP, QUIT
 import random
 import time
 pygame.init()
 
-# 初期変数の設定
-# x,y=pyautogui.size()
 x, y = 800, 600
 font = pygame.font.Font(None, 300)
-
-
-def keybutton(sur, nums, key):  # ボタン操作
-    for event in pygame.event.get():
-        if event.type == KEYDOWN:
-            if event.key == K_ESCAPE:
-                pygame.quit()
-                sys.exit()
-            elif event.key == K_SPACE:
-                key = False
-        elif event.type == KEYUP:
-            key = True
-        return key
-
-
-def drawing():
-    sur = pygame.display.set_mode((x, y))
-    sur.fill((0, 0, 0))
-    return sur
+sur = pygame.display.set_mode((x, y))
+fps = pygame.time.Clock()
 
 
 def getnum():
-    x = []
-    for i in range(1, 73):
-        x.append(i)
+    x = [i for i in range(1, 73)]
     return x
 
 
 def change_number(sur, nums):  # nums[1-72]
     random.seed()
-    num = random.sample(nums, 1)
+    num = random.sample(nums, 1)[0]
     text = font.render(str(num), True, (255, 255, 255))
-    sur.blit(text, [x / 2 - 200, y / 2 - 100])
-    time.sleep(0.05)
+    sur.blit(text, [x / 2 - 100, y / 2 - 100])
     pygame.display.update()
 
 
@@ -52,18 +30,30 @@ def callchange_number(sur, nums, call):
     if(call == True):
         change_number(sur, nums)
     else:
-        num = random.sample(nums, 1)
+        num = random.sample(nums, 1)[0]
         text = font.render(str(num), True, (255, 255, 255))
-        sur.blit(text, [x / 2 - 150, y / 2 - 50])
+        sur.blit(text, [x / 2 - 100, y / 2 - 100])
+
+
+def mainloope(nums, call=None):
+    while True:
+        sur.fill((0, 0, 0))
+        for event in pygame.event.get():
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    pygame.quit()
+                    sys.exit()
+                elif event.key == K_SPACE:
+                    call = False
+                elif event.key == K_UP:
+                    call = True
+        callchange_number(sur, nums, call)
+        fps.tick(30)
 
 
 def main():
     nums = getnum()
-    key = None
-    while True:
-        sur = drawing()
-        call = keybutton(sur, nums, key)
-        callchange_number(sur, nums, call)
+    mainloope(nums)
 
 
 main()
