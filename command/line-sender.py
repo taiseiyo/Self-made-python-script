f4dd775a4f5c00ff7bf4f96e6ac728e464b51e7e
taiseiyo <53172326+taiseiyo@users.noreply.github.com>
2020-02-22 20:31:59 +0900

    fix line-sender.py

--- command/line-sender.py
+++ command/line-sender.py
@@ -6,10 +6,17 @@ from subprocess import PIPE
 
 class Bot(object):
     def __init__(self):
-        self.line = Line(token="***")
+        self.line = Line(token="DVxKcN9dg0ecy9ETJtRpVHZr2Uouw1IyRDQtfxl3Ypk")
 
     def sender(self):
         self.line.post(message="finish experimt")
+        self.stopper()
+
+    def stopper(self):
+        command = "sudo service cron start"
+        proc = subprocess.Popen(
+            command, shell=True, stderr=PIPE, stdout=PIPE, text=True)
+        proc.communicate()[0]
 
 
 def switch():
