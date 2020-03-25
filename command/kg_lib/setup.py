6557036f0e1548adcfcf45aa566af8601b6c8dcf
taiseiyo <53172326+taiseiyo@users.noreply.github.com>
2020-03-25 14:27:31 +0900

    fix

--- command/kg_lib/setup.py
+++ command/kg_lib/setup.py
@@ -3,11 +3,11 @@ from setuptools import setup, find_packages
 
 setup(
     name="kg_lib",
-    version="0.0.1",
+    version="0.0.3",
     description="search book in kwansei gakuin library",
     author="suzuki taisei",
     author_email="taiseiyo11@gmail.com",
-    url="https://github.com/taiseiyo/Self-made-python-script/tree/master/command/kglib/",
+    url="https://github.com/taiseiyo/Self-made-python-script/tree/master/command/kg_lib/",
     packages=find_packages(),
     include_package_data=False,
     install_requires=[
